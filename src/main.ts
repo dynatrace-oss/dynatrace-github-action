868e0fbc7ad2c6c842e0be0c1f5f400120a37a0a
Wolfgang Beer <wolfgang@smartlab.at>
2021-01-06 09:57:03 +0100

    dep

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const url: string = core.getInput('url')
+    core.debug(`hello dynatrace`) 
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    
 
-    core.setOutput('time', new Date().toTimeString())
+    
   } catch (error) {
     core.setFailed(error.message)
   }
