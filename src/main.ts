2a3c3757332504af45ee6976e9b7df34c9bff141
Wolfgang Beer <wolfgang@smartlab.at>
2021-01-07 12:53:03 +0100

    Empty metrics fixing

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,14 @@ async function run(): Promise<void> {
     const token: string = core.getInput('token');
 
     const mStr = core.getInput('metrics');
+    core.info(mStr)
     if(mStr.length > 5) {
       const metrics = yaml.load(mStr) as d.Metric[];
       d.sendMetrics(url, token, metrics);
     }
     
     const eStr = core.getInput('events');
+    core.info(eStr)
     if(eStr.length > 5) {
       const events = yaml.load(eStr) as d.Event[];
       d.sendEvents(url, token, events);
