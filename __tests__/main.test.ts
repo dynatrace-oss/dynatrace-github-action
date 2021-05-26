4dc7be86b4415805db1b2eade7b0920c3ecbae63
Wolfgang Beer <wolfgang@smartlab.at>
2021-05-26 10:21:28 +0200

    removed dummy tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -12,19 +12,6 @@ import * as cp from 'child_process';
 import * as path from 'path';
 import * as dt from '../src/dynatrace';
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10);
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number');
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end.getTime() - start.getTime());
-  expect(delta).toBeGreaterThan(450);
-})
-
 test('valid metrik key with point', async () => {
   expect(dt.safeMetricKey("metric.key")).toEqual("metric.key");
 })
