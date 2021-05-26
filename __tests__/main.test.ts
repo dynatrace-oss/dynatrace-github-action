a25c6c83972c0cb00c27dd8f7b974b6961934fed
Wolfgang Beer <wolfgang@smartlab.at>
2021-05-26 10:28:10 +0200

    Update main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,16 +6,3 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
-import {wait} from '../src/wait';
-import * as process from 'process';
-import * as cp from 'child_process';
-import * as path from 'path';
-import * as dt from '../src/dynatrace';
-
-test('valid metrik key with point', async () => {
-  expect(dt.safeMetricKey("metric.key")).toEqual("metric.key");
-})
-
-test('invalid metrik key chars conversion', async () => {
-  expect(dt.safeMetricKey("metric.k&y")).toEqual("metric.k_y");
-})
